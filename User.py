f80596b84bec0649348f46b8beae74fcbfaf9bcf
Eric Orozco <wookiee323@gmail.com>
2017-05-02 22:34:29 -0700

    User class integrated with login page

--- User.py
+++ User.py
@@ -4,22 +4,15 @@ Created on May 1, 2017
 @author: wookiee
 '''
 
-class User(object):
-    '''
-    This is a user object.  It automagically hashes passwords passed to it.
-    For now, I am including the passwd field because we have a database full of
-    unhashed passwords.
-    '''
-    def __init__(self, fn, ln, account_type, email, password=None):
-        self.firstname = fn
-        self.lastname = ln
-        self.account_type = account_type
-        self.email = email
-        self.passwd = password
-        # self.set_password(password)
+class User:
+    """
+    This is the user object which holds personal login
+    information including an ID for their perspective
+    custom team they created
+    """
 
-    #def set_password(self, password):
-        #self.pw_hash = generate_password_hash(password)
-
-    #def check_password(self, password):
-        #return check_password_hash(self.pw_hash, password)
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        #self.favoriteTeam = favoriteTeam
+        #self.customID = customID
